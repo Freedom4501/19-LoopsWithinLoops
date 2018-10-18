3a41fdd83d61575ca2150363ef49b9dd43604a6d
lim5 <lim5@rose-hulman.edu>
2018-10-18 17:11:01 -0400

    Mashengjun Li coming in hot!!!!

--- src/m6_nested_loops_in_graphics.py
+++ src/m6_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Mashengjun Li.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -83,6 +83,22 @@ def draw_L(window, circle, r, c):
     # TODO: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    radius = circle.radius
+    original_x = circle.center.x
+    original_y = circle.center.y
+    x = original_x
+    y = original_y
+    for k in range(r):
+        for j in range(3):
+            new_circle = rg.Circle(rg.Point(x,y), radius)
+            new_circle.attach_to(window)
+            window.render(0.1)
+            x = x + 2 * radius
+        y = y + 2 * radius
+        x = circle.center.x
+
+
+
 
 
 def run_test_draw_wall_on_right():
